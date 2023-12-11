a0a307dbb4de3ab0086e419f32a7a442a04fd94a
Alexey <kabzone23@mail.ru>
2023-12-11 11:27:46 +0300

    hw3

---
+++ HW3/user_repository.py
@@ -0,0 +1,13 @@
+class UserRepository:
+
+    data = []
+
+    def add_user(self, user):
+        self.data.append(user)
+
+    def all_users_logout(self):
+        dataNew = []
+        for elem in self.data:
+            if elem.admin:
+                dataNew.append(elem)
+        self.data = dataNew
